31a16bc9285cf9387f4b3e240a5075ffcb49e0b2
SergioLaptop <al286319@uji.es>
2019-10-25 18:56:26 +0200

    ui a medias

--- Assets/Scripts/Life.cs
+++ Assets/Scripts/Life.cs
@@ -7,13 +7,26 @@ public class Life : MonoBehaviour
 {
     // Start is called before the first frame update
 
-    public int calidad_ambiente_inicial = 100;
+    public int contaminacion_inicial = 0;
+    public int contaminacion;
     public int electricidad_inicial = 100;
     public int agua_inicial = 100;
     public Slider calidadSlider;
     public Slider elecSlider;
     public Slider aguaSlider;
+    public float time;
 
+    private void Awake()
+    {
+        contaminacion = contaminacion_inicial;
+        time = Time.time;
+
+    }
+
+    public void contamina()
+    {
+        
+    }
 
     void Start()
     {
