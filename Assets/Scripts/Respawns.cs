395ebe96f16f5e5fadb3f0f07e9422ca0d89d5f1
NietakO <al316458@uji.es>
2019-10-26 01:35:46 +0200

    .

--- Assets/Scripts/Respawns.cs
+++ Assets/Scripts/Respawns.cs
@@ -4,25 +4,20 @@ using UnityEngine;
 
 public class Respawns : MonoBehaviour
 {
-    public GameObject[] edificios;
-    public Vector3[,] spawnValues;
-    public int large;
+    public List<GameObject> models = new List<GameObject>();
+    
     public float spawnWait;
-    public float spawnMaxWait;
-    public float spawnMinWait;
-    public int startWait;
-    bool found = false;
+    public float spawnMaxWait=5;
+    public float spawnMinWait=15;
     public bool end;
-    int size;
-    List<GameObject> grid;
-   GameObject gridItem;
-    public GameObject respawnee;
-
+    List<GameObject> grid = Global.grid;
+    GameObject gridItem;
+    List<GameObject> buildings;
     // Start is called before the first frame update
+
+
     void Start()
     {
-        spawnValues = new Vector3[10,10];
-        size = 0;
 
     }
 
@@ -34,12 +29,14 @@ public class Respawns : MonoBehaviour
 
     IEnumerator waitSpawner()
     {
-        yield return new WaitForSeconds(startWait);
+        
         while (!end)
         {
+            yield return new WaitForSeconds(Random.Range(spawnMinWait,spawnMaxWait));
             int rand = Random.Range(0, grid.Capacity);
             gridItem = grid[rand];
-            Instantiate(respawnee, gridItem.transform.position, gridItem.transform.rotation);
+            //en caso de que queramos que no se meta el primer modelo de casa, meter Random en el instantiate.
+            buildings.Add(Instantiate(models[0], gridItem.transform.position, gridItem.transform.rotation));
             grid.Remove(gridItem);
             //Destroy
         }
