3f6437087ade0c83da827add648e884b72d6ff6d
SergioLaptop <al286319@uji.es>
2019-10-25 16:56:32 +0200

    un cubo para usted

---
+++ Assets/Scripts/Life.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Life : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public int calidad_ambiente_inicial = 100;
+    public int electricidad_inicial = 100;
+    public int agua_inicial = 100;
+    public Slider calidadSlider;
+    public Slider elecSlider;
+    public Slider aguaSlider;
+
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
