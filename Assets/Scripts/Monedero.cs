290054ead3427804d06e4128777953aaaeeabbc7
SergioLaptop <al286319@uji.es>
2019-10-26 23:54:20 +0200

    monedero fet

---
+++ Assets/Scripts/Monedero.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Monedero : MonoBehaviour
+{
+
+    public Text dinero;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    public void contarDinero()
+    {
+        dinero.text = "" + Global.money;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        contarDinero();
+    }
+}
