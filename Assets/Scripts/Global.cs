49e06e776c1397aaafda71c0d3ceb0d4bbb67953
Alejandro Gas Ventura <al315562@uji.es>
2019-10-26 17:38:32 +0200

    ending scripts

--- Assets/Scripts/Global.cs
+++ Assets/Scripts/Global.cs
@@ -8,5 +8,6 @@ using UnityEngine;
 	 public static List<GameObject> houses =  new List<GameObject>();
      public static int money = 500;
      
+	 //TODO Añadir la primera casa al listado 
      
  }
