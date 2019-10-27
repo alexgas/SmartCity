e96183b3a619b1dacaa74fc08828dfedbb90de21
SergioLaptop <al286319@uji.es>
2019-10-27 03:34:27 +0100

    final del tó

---
+++ Assets/Scripts/LevelManager.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.SceneManagement;
+
+public class LevelManager : MonoBehaviour, IPointerDownHandler {
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    //void OnPointerDown(PointerEventData ped)
+    //{
+    //    Debug.Log("Escena due");
+    //    SceneManager.LoadScene("SmartCityFinal");
+    //}
+
+    public void CargaNivel(string nombreNivel){
+		SceneManager.LoadScene(nombreNivel);
+	}
+	public void Salir () {
+		Application.Quit();
+	}
+
+    void IPointerDownHandler.OnPointerDown(PointerEventData eventData)
+    {
+        Debug.Log("Escena due");
+        SceneManager.LoadScene("SmartCityFinal");
+        throw new System.NotImplementedException();
+    }
+}
